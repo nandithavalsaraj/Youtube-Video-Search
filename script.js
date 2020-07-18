0f0536583bde6e014816190c1ce9a45af4695c1b
Nanditha Valsaraj <40928820+nandithavalsaraj@users.noreply.github.com>
2020-07-19 00:35:26 +0530

    fetch videos using keyword
    Display the keyword related videos on the basis of the user defined order

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	var API_KEY = "AIzaSyBXntONNCIFdoJjSX84Uo-s6pCOxSJv06U";
+	
+	var video=''
+
+	$('#form').submit(function(event){
+		event.preventDefault()
+		alert("form is submitted")
+		var search = $("#search").val()
+		var maxResults = $("#maxResults").val()
+		var orderInput = $("#order-input").val()
+		videoSearch(API_KEY,search,parseInt(maxResults), orderInput)
+	
+	})
+
+	function videoSearch(key, search, maxResults, orderInput){
+		$.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search +"&order=" + orderInput, function(data){
+			console.log(data)
+			data.items.forEach(item =>{
+				video = `
+				<iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+
+				`
+				$("#videos").append(video)
+				})
+		})
+	}
+})
